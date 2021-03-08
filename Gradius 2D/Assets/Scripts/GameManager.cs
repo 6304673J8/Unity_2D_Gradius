5c63f4a900f786516fab8b9d15d8e28860f8e729
Georghe Julca <petergeorghe.julca@enti.cat>
2021-03-08 10:40:42 +0100

    Pixel Perfect Setup
    Setting tilemap

---
+++ Gradius 2D/Assets/Scripts/GameManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance { get; private set; }
+    public int score = 0;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(this);
+        } else
+        {
+            Debug.Log("WARNING: multiple " + this + " in scene!");
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
