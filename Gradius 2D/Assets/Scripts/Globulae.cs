41174556c0a104414b2ec6f6b233f8c5e3b4bf10
Georghe Julca <petergeorghe.julca@enti.cat>
2021-03-11 19:33:52 +0100

    New Enemy Behaviour
    First step before making spawner of enemies

---
+++ Gradius 2D/Assets/Scripts/Globulae.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Globulae : MonoBehaviour
+{
+    public Transform player;
+    public float moveSpeed = 1f;
+    public float range;
+    private Rigidbody2D enemyrb;
+    private Vector2 movement;
+
+    void Start()
+    {
+        enemyrb = this.GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        Vector3 direction = player.position - transform.position ;
+        //direction.Normalize();
+        movement = direction;
+
+        //Comprobar la distancia entre el enemigo y el jugador
+        float distanceToPlayer = Vector3.Distance(transform.position, player.position);
+        /*if (distanceToPlayer < range)
+        {
+            Destroy(gameObject);
+        }*/
+    }
+
+    private void FixedUpdate()
+    {
+        MoveEnemy(movement);
+    }
+
+    void MoveEnemy(Vector2 direction)
+    {
+        enemyrb.MovePosition((Vector2)transform.position + (direction * moveSpeed * Time.deltaTime));
+    }
+}
